aaff10fc44a74f6cbef77cad189b43b2c0e5f657
Tayana <tayana1498@gmail.com>
2023-02-14 13:19:50 -0700

    testing

---
+++ src/components/department/IndexDepartment.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+
+const IndexDepartment = () => {
+  // State to store the departments data
+  const [departments, setDepartments] = useState([]);
+  
+  // State to store the number of departments to display
+  const [displayCount, setDisplayCount] = useState(8);
+
+  // Use effect to fetch the departments data from the API
+  useEffect(() => {
+    fetch('https://openaccess-api.clevelandart.org/api/departments')
+      .then((response) => response.json())
+      .then((data) => setDepartments(data));
+  }, []); // Second argument of an empty array means this useEffect will only run once on component mount
+
+  // Function to handle the "handleAllArtworks" button click
+  const handleAllArtworks = () => {
+    // Update displayCount to display all departments
+    setDisplayCount(departments.length);
+  };
+
+  return (
+    <div>
+      <h1>View Artworks</h1>
+      <p>Search by Department</p>
+      <ul>
+   
+        {departments.slice(0, displayCount).map((department) => (
+          <li key={department.departmentID}>{department.displayName}</li>
+        ))}
+      </ul>
+
+      {displayCount < departments.length && (
+        <button onClick={handleAllArtworks}>See All Artworks</button>
+      )}
+    </div>
+  );
+};
+
+export default IndexDepartment;
