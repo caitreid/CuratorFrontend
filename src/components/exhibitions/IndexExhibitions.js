f849eada3ddcbc646225ba985d07a38bc64021c0
caitreid <ctlnrd@gmail.com>
2023-02-15 13:51:55 -0500

    index exhibition template

---
+++ src/components/exhibitions/IndexExhibitions.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react"
+import Card from 'react-bootstrap/Card'
+
+
+const IndexExhibitions = (props) => {
+
+
+    return (
+        <>
+            <h1>Index Exhibitions</h1>
+        </>
+        
+    )
+
+}
+
+export default IndexExhibitions;
