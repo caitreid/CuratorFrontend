b60173862a15c54e9064357f729a156cf170cd5b
caitreid <ctlnrd@gmail.com>
2023-02-16 10:08:02 -0500

    clean up index dept

--- src/components/departments/IndexDepartments.js
+++ src/components/departments/IndexDepartments.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import { getAllDepartments } from '../../api/departments'
 import Card from 'react-bootstrap/Card'
 
@@ -9,26 +8,14 @@ const IndexDepartment = (props) => {
   console.log('this is departments', departments)
   console.log('this is props', props)
   
-  // State to store the number of departments to display
-  //const [displayCount, setDisplayCount] = useState(8);
 
-  //Use effect to fetch the departments data from the API
+  // Use effect to fetch the departments data from the API
   useEffect(() => {
     getAllDepartments() 
       .then(res => setDepartments(res.data.departments))
       .catch(err => console.log(err))
-}, []); // Second argument of an empty array means this useEffect will only run once on component mount
-console.log('this is department', departments)
-  // Function to handle the "handleAllArtworks" button click
- // const handleAllArtworks = () => {
-    // Update displayCount to display all departments
- //   setDisplayCount(departments.length);
- // };
-
-// if error, display an error
-// if (error) {
-//   return <p>Error!</p>
-// }
+  }, []); // Second argument of an empty array means this useEffect will only run once on component mount
+  console.log('this is department', departments)
 
 if (!departments) {
   // if no pets loaded yet, display 'loading'
@@ -38,49 +25,24 @@ if (!departments) {
   return <p>No departments yet, go add some!</p>
 }
 
-const departmentCards = departments.map(department => (
-  <Card key={department.id} style={{ backgroundImage: `url(${department.img})`, width: "250px" }}>
-    <Card.Header>
-      {department.name}
-    </Card.Header>
-  </Card>
-))
-return (
-  <div style={{
-      display: "grid",
-      gridTemplateColumns: "repeat(3, 1fr)",
-      gap: "1rem",
-      height: "100vh",
-      overflow: "scroll"
-    }}>
-    {departmentCards}
-  </div>
-)
-
+  const departmentCards = departments.map(department => (
+    <Card key={department.id} style={{ backgroundImage: `url(${department.img})`, width: "250px" }}>
+      <Card.Header>
+        {department.name}
+      </Card.Header>
+    </Card>
+  ))
   return (
-    
-    <div>
-      <h1>View Artworks</h1>
-      <p>Search by Department</p>
-       {departmentCards}
-
-
-
-
-      {/* <ul>
-   // Need to somehow display department images with their corresponding names, ex: Asian Art 
-        {departments.slice(0, displayCount).map((department) => (
-          <li key={department.departmentID}>{department.displayName}</li>
-        ))}
-      </ul>
-
-      {displayCount < departments.length && (
-        <button onClick={handleAllArtworks}>See All Artworks</button>
-      )} */}
-
+    <div style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 1fr)",
+        gap: "1rem",
+        height: "100vh",
+        overflow: "scroll"
+      }}>
+      {departmentCards}
     </div>
-    
-  );
+  )
 };
 
 export default IndexDepartment;
