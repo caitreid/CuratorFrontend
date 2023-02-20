a37006cc7a1529012e9386999fcc42a6eb2e0f3b
thepjos <josh.thephasdin@gmail.com>
2023-02-20 12:55:02 -0600

    fixed index code break

--- src/components/exhibitions/IndexExhibition.js
+++ src/components/exhibitions/IndexExhibition.js
@@ -39,88 +39,45 @@ const IndexExhibition = (props) => {
 
   const exhibitionCards = exhibitions.map(exhibition => (
     <div key={exhibition.id}>
+      <Link to={`/exhibitions/${exhibition._id}`} style={{textDecoration: 'none'}}>
+          <Card
+            className="department__card"
+              key={exhibition.id}
+              
+              style={{
+                margin: 20,
+                boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+                borderRadius: "10px",
+                overflow: "hidden",
+                width: '250px',
+                height: '200px'
 
-
-    <Link to={`/exhibitions/${exhibition._id}`} style={{textDecoration: 'none'}}>
-        <Card
-        className="department__card"
-          key={exhibition.id}
-          
-          style={{
-            margin: 20,
-            boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
-            borderRadius: "10px",
-            overflow: "hidden",
-            width: '250px',
-            height: '200px'
-
-        }}
-      >
-        <div
-          style={{
-            
-            backgroundImage: `url(${exhibition.img})`,
-            backgroundSize: "cover",
-            height: "250px",
-            width: '300px'
-          }}
-<<<<<<< HEAD
-        >
-          
-
-        </div>
-      </div>
-    </Card>
-    <div className='ms-4 mb-4' style={{ color: 'black' }}>
-            <div>{exhibition.startDate} - {exhibition.endDate}</div>
-    <h2
-          style={{
-            fontWeight: "bold",
-            fontSize: "18px",
-            marginBottom: "5px"
-=======
-        />
-        <div
-          style={{
-            padding: "5px",
-            height: "10%",
-            position: "relative"
->>>>>>> main
-          }}
-        >
-
-          <div
-            style={{
-              position: "absolute",
-              bottom: "20px",
-              left: "20px",
-              color: "#000",
-              fontSize: "14px",
-              fontWeight: "bold",
-              display: "flex",
-              alignItems: "center"
             }}
           >
-            
-
-          </div>
-        </div>
-      </Card>
-      <div className='ms-4 mb-4'>
-              <div>{exhibition.startDate}</div>
-              <div>{exhibition.endDate}</div>
-      <h2
-            style={{
-              fontWeight: "bold",
-              fontSize: "18px",
-              marginBottom: "5px",
-              color: "#000"
-            }}
-          >
-            {exhibition.title}
-      </h2>
-      </div>
-      </Link>
+            <div
+              style={{
+                
+                backgroundImage: `url(${exhibition.img})`,
+                backgroundSize: "cover",
+                height: "250px",
+                width: '300px'
+              }}>     
+            </div>
+        </Card>
+            <div className='ms-4' style={{ color: 'black' }}>
+                    <div>{exhibition.startDate} - {exhibition.endDate}</div>
+            </div>
+              <h2 className='ms-4 mb-4'
+                    style={{
+                      fontWeight: "bold",
+                      fontSize: "18px",
+                      marginBottom: "5px",
+                      color: "#000"
+                    }}
+                  >
+                    {exhibition.title}
+              </h2>
+        </Link>
     </div>
   )
   );
